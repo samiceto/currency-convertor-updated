4884d7e62c78d273be85aa9ce3989b08667d7fc1
samiceto <you@example.com>
2024-04-26 00:10:05 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.92,
+    GBP: 0.72,
+    INR: 74.4,
+    PKR: 280
+};
+let student_answer = await inquirer.prompt([{
+        name: "from",
+        type: "list",
+        message: "pleas type your from currency",
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: "to",
+        type: "list",
+        message: "pleas type your from currency",
+        choices: ['USD', 'EUR', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: "amount",
+        type: "number",
+        message: "please type the amount you want to convert"
+    }]);
+let fromRate = currency[student_answer.from];
+let toRate = currency[student_answer.to];
+let amountConvert = student_answer.amount;
+let baseCurrency = amountConvert / fromRate;
+let convertedAmount = baseCurrency * toRate;
+console.log(convertedAmount);
